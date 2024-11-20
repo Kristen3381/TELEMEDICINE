4e53d19086f0ea45692143a20fa67e2e78b59afd
kristen3381 <shavia.laura462@gmail.com>
2024-11-21 02:00:42 +0300

    my first cpmmit

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    $('.fa-bars').click(function(){
+        $(this).toggleClass('fa-times')
+        $('.navbar').toggleClass('nav-toggle')
+    });
+    $(Window).on('scroll load',function(){
+        $(this).removeClass('fa-times')
+        $('.navbar').removeClass('nav-toggle')
+        if($(Window).scrollTop()>30){
+            $('header').addClass('header-active ')
+        }else{
+            $('header').removeClass('header-active ')
+        }
+    });
+});
